3295adac1ae7eb948af2331c122551912b5cd977
YanisDj <yanisdjahnit03@gmail.com>
2023-04-11 16:03:51 +0200

    Mise en place de la méthode pour les les statistiques du questionnaire

--- src/fr/iut/montreuil/S4_R02_2023_05_SuperQuizz_sint/modeles/IServiceSuperQuizzQuestionnaire.java
+++ src/fr/iut/montreuil/S4_R02_2023_05_SuperQuizz_sint/modeles/IServiceSuperQuizzQuestionnaire.java
@@ -1,6 +1,7 @@
 package fr.iut.montreuil.S4_R02_2023_05_SuperQuizz_sint.modeles;
 
-import fr.iut.montreuil.S4_R02_2023_05_SuperQuizz.questionnaire_sme.entities.dto.BilanStatsDTO;
+
+import fr.iut.montreuil.S4_R02_2023_05_SuperQuizz.questionnaire_sme.entities.dto.QuestionnaireDTO;
 import fr.iut.montreuil.S4_R02_2023_05_SuperQuizz_sint.entities.BestOfQuestionnaireDTO;
 import utils.exceptions.NombreDeFoisJoueExceptions;
 import utils.exceptions.NombreReponseExceptions;
@@ -8,5 +9,5 @@ import utils.exceptions.PasDeStatsExceptions;
 
 public interface IServiceSuperQuizzQuestionnaire {
 
-    public BestOfQuestionnaireDTO bestOfQuestionnaire(BilanStatsDTO bilanStatsDTO) throws PasDeStatsExceptions, NombreDeFoisJoueExceptions, NombreReponseExceptions;
+    public BestOfQuestionnaireDTO bestOfQuestionnaire(QuestionnaireDTO questionnaireDTO) throws PasDeStatsExceptions, NombreDeFoisJoueExceptions, NombreReponseExceptions;
 }
--- src/fr/iut/montreuil/S4_R02_2023_05_SuperQuizz_sint/tests/test.java
+++
@@ -1,5 +0,0 @@
-package fr.iut.montreuil.S4_R02_2023_05_SuperQuizz_sint.tests;
-
-public class test {
-
-}
