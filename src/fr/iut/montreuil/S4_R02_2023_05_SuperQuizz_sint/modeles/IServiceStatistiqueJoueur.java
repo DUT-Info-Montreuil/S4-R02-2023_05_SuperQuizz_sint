ddca14562f4630672c39c199853b43336fa152f6
bseydi <boulaye.seydi@gmail.com>
2023-04-16 15:34:03 +0200

    Modification du paramètre de recupererStatistiques de l'interface IServiceStatistiqueJoueur

--- src/fr/iut/montreuil/S4_R02_2023_05_SuperQuizz_sint/modeles/IServiceStatistiqueJoueur.java
+++ src/fr/iut/montreuil/S4_R02_2023_05_SuperQuizz_sint/modeles/IServiceStatistiqueJoueur.java
@@ -4,12 +4,12 @@ package fr.iut.montreuil.S4_R02_2023_05_SuperQuizz_sint.modeles;
 import java.util.Collection;
 import java.util.TreeSet;
 
-import fr.iut.montreuil.S4_R02_2023_05_SuperQuizz.joueur_sme.entities.dto.JoueurDTO;
 import fr.iut.montreuil.S4_R02_2023_05_SuperQuizz.joueur_sme.entities.dto.PartieJoueurDTO;
+import fr.iut.montreuil.S4_R02_2023_05_SuperQuizz.joueur_sme.impl.ServiceJoueurImpl;
 
 public interface IServiceStatistiqueJoueur {
 	
-	public Collection<PartieJoueurDTO> recupererStatistiques(JoueurDTO joueur);
+	public Collection<PartieJoueurDTO> recupererStatistiques(ServiceJoueurImpl joueur);
 	
 	public TreeSet<PartieJoueurDTO> trierPartie(Collection<PartieJoueurDTO> parties);
 	
